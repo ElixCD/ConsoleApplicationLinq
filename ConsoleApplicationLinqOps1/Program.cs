b2e908c9ac13607052654bb06d7df2d4558f02a4
elix <elix.cd@gmail.com>
2017-11-10 12:04:52 -0600

    Joins

--- ConsoleApplicationLinqOps1/Program.cs
+++ ConsoleApplicationLinqOps1/Program.cs
@@ -17,14 +17,14 @@ namespace ConsoleApplicationLinqOps1
             // Declaramos (y cumplimentamos) el tipo anónimo
             var tipoAnonimo = new { PropiedadEntera = 1, PropiedadCadena = "cadena" };
 
-                        // Mostramos por pantalla su contenido
+            // Mostramos por pantalla su contenido
             Console.WriteLine(String.Format("El contenido del tipo anónimo es {0} y {1}",
                 tipoAnonimo.PropiedadEntera, tipoAnonimo.PropiedadCadena));
 
             Console.WriteLine("\n================= SELECT ==================");
 
             var listaNombreFechaClientes01 = from c in DataLists.ListaClientes
-                                           select c.Nombre;
+                                             select c.Nombre;
 
             foreach (var nombreFecha in listaNombreFechaClientes01)
                 Console.WriteLine(string.Format("El cliente {0} ", nombreFecha));
@@ -32,7 +32,7 @@ namespace ConsoleApplicationLinqOps1
             Console.WriteLine("\n================= SELECT VARIOS CAMPOS ==================");
 
             var listaNombreFechaClientes02 = from c in DataLists.ListaClientes
-                                           select new { NombreCliente = c.Nombre, FechaNacimiento = c.FechaNac };
+                                             select new { NombreCliente = c.Nombre, FechaNacimiento = c.FechaNac };
 
             foreach (var nombreFecha in listaNombreFechaClientes02)
                 Console.WriteLine(string.Format("El cliente {0} nació el {1}",
@@ -56,6 +56,26 @@ namespace ConsoleApplicationLinqOps1
 
             Console.WriteLine("\n================= SELECT VARIOS OBJETOS ==================");
 
+            var listaPedidosClientes = from c in DataLists.ListaClientes    // Lista de clientes
+                                       from p in DataLists.ListaPedidos     // Lista de pedidos
+                                       where p.IdCliente == c.Id            // Filtro: ID del pedido == ID del cliente
+                                       select new { NumPedido = p.Id, FechaPedido = p.FechaPedido, NombreCliente = c.Nombre };
+
+            foreach (var pedidoCliente in listaPedidosClientes)
+            {
+                Console.WriteLine(string.Format("El pedido {0} enviado en {1} pertenece a {2}.", pedidoCliente.NumPedido, pedidoCliente.FechaPedido, pedidoCliente.NombreCliente));
+            }
+
+            Console.WriteLine("\n================= SELECT VARIOS OBJETOS JOIN ==================");
+
+            listaPedidosClientes = from c in DataLists.ListaClientes    // Lista de clientes
+                                   join p in DataLists.ListaPedidos on c.Id equals p.IdCliente  // Equivale al INNER JOIN TABLA2 ON TABLA1.ID_PK = TABLA2.ID_FK
+                                   select new { NumPedido = p.Id, FechaPedido = p.FechaPedido, NombreCliente = c.Nombre };
+
+            foreach (var pedidoCliente in listaPedidosClientes)
+            {
+                Console.WriteLine(string.Format("El pedido {0} enviado en {1} pertenece a {2}.", pedidoCliente.NumPedido, pedidoCliente.FechaPedido, pedidoCliente.NombreCliente));
+            }
 
             Console.ReadKey();
         }
